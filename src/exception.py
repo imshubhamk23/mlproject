770328314f0ada3e2e1fc6ab15a417d8b696ed1d
Shubham Kolekar <kolekar.shubham95@gmail.com>
2023-04-12 17:16:46 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import sys
+import logger
+
+def error_message_details(error,error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"error occured at python script [{filename}] at line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
+
+    return error_message
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
+
+# if __name__=="__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logger.logging.info(CustomException(e,sys))
+#         raise CustomException(e,sys)
